11577eb5235b4b5d7ee26d3b6e2e3ad50400345b
Seongmin Choi <chsm0115@gmail.com>
2025-10-21 16:07:08 +0900

    Initial commit

---
+++ src/test/java/com/erp/erp_back/ErpBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erp.erp_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
