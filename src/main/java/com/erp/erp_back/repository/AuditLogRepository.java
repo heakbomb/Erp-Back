60946c04183272062855837e8969b43fe611bdc5
Seongmin Choi <chsm0115@gmail.com>
2025-10-23 19:14:41 +0900

    Create repository

---
+++ src/main/java/com/erp/erp_back/repository/AuditLogRepository.java
@@ -0,0 +1,11 @@
+package com.erp.erp_back.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.erp.erp_back.entity.AuditLog;
+
+@Repository
+public interface AuditLogRepository extends JpaRepository<AuditLog, Long> {
+    // 기본적인 CRUD 메소드가 이미 모두 구현되어 있음
+}
